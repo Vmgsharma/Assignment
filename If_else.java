650683183ed8747eaa0d952b20d62c6c1725bd55
Vmgsharma <vmgsharma01@gmail.com>
2017-06-28 19:13:20 -0600

    second branch commit

---
+++ If_else.java
@@ -0,0 +1,14 @@
+
+public class If_else {
+public static void main(String args[]){
+	
+	int i = 0;
+	
+	if(i==0){
+		
+		System.out.println("Hello World");
+	}
+	else
+		System.out.println("Hello America");
+}
+}
---
+++ Switch.java
@@ -0,0 +1,18 @@
+
+public class Switch {
+public static void main (String args[]){
+	int color_code = 1;
+	switch(color_code){
+	case 1 :
+		System.out.println("Red");
+		break;
+	case 2 :
+		System.out.println("Orange");
+		break;
+	case 3 :
+		System.out.println("White");
+		break;
+		}
+	
+}
+}
