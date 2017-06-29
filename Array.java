1378da1d7cad6cdd27f0586c01af05c660cbed9e
Vmgsharma <vmgsharma01@gmail.com>
2017-06-28 19:12:39 -0600

    first main branch commit

---
+++ Array.java
@@ -0,0 +1,16 @@
+
+public class Array {
+	public static void main(String args[]){
+		
+	
+	int arr[] ={70,80,90,70,55};
+	
+	for(int i =0;i<5; i++)
+	{
+		
+		System.out.println(arr[i]);
+	}
+	
+	}
+
+}
